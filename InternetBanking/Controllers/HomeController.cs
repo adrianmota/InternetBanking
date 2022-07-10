acbd88e0325d996acad4ce3f5772c23c7d7e65bc
Adrian Mota <adrianmota152@gmail.com>
2022-07-10 16:49:43 -0400

    First commit

---
+++ InternetBanking/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using InternetBanking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetBanking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
