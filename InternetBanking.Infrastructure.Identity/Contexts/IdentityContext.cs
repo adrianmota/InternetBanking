832cc7453d19739a149627f3cfd0e433eac70e77
Adrian Mota <adrianmota152@gmail.com>
2022-07-12 20:34:51 -0400

    Add Identity Layer

---
+++ InternetBanking.Infrastructure.Identity/Contexts/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using InternetBanking.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBanking.Infrastructure.Identity.Contexts
+{
+    public class IdentityContext : IdentityDbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+    }
+}
