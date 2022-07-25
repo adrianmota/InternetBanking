bd3e56343fafaaf699b9f4bb3865a42e04041a04
Adrian Mota <adrianmota152@gmail.com>
2022-07-25 11:35:03 -0400

    Refactor JS Code and change target action in home button

--- InternetBanking/wwwroot/js/AdminUser.js
+++ InternetBanking/wwwroot/js/AdminUser.js
@@ -1,19 +1,20 @@
-﻿let userId;
+﻿document.addEventListener('DOMContentLoaded', () => {
+    let userId;
 
-const activeUser = document.querySelector("#btnActiveUser")
-const deactiveUser = document.querySelector("#btnDeactiveUser")
+    const activeUser = document.querySelector("#btnActiveUser")
+    const deactiveUser = document.querySelector("#btnDeactiveUser")
 
+    const setUser = id => userId = id;
 
-const setUser = id => userId = id;
+    const setState = () => {
+        const http = window.location.protocol;
+        const domain = window.location.host;
+        const url = `${http}//${domain}/User/SetUserStatus?id=${userId}`;
 
-const setState = () => {
-    const http = window.location.protocol;
-    const domain = window.location.host;
-    const url = `${http}//${domain}/User/SetUserStatus?id=${userId}`;
+        console.log(url)
+        window.location = url;
+    }
 
-    console.log(url)
-    window.location = url;
-}
-
-activeUser.addEventListener("click", () => { setState() });
-deactiveUser.addEventListener("click", () => { setState() });
+    activeUser.addEventListener("click", () => { setState() });
+    deactiveUser.addEventListener("click", () => { setState() });
+});
--- InternetBanking/wwwroot/js/SaveProduct.js
+++ InternetBanking/wwwroot/js/SaveProduct.js
@@ -1,21 +1,19 @@
-﻿console.log("iygseuifn")
-const limitInput = document.getElementById('limit-input');
-const amountInput = document.getElementById('amount-input');
-const productType = document.getElementById('product-type-select');
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const limitInput = document.getElementById('limit-input');
+    const amountInput = document.getElementById('amount-input');
+    const productType = document.getElementById('product-type-select');
 
-
-
-
-if (productType != null) {
-    productType.onclick = () => {
-        const productTypeSelected = productType.options[productType.selectedIndex].value;
-        if (productTypeSelected == 3) {
-            limitInput.hidden = false;
-            amountInput.hidden = true;
-        } else {
-            limitInput.hidden = true;
-            amountInput.hidden = false;
-            console.log(amountInput)
+    if (productType != null) {
+        productType.onclick = () => {
+            const productTypeSelected = productType.options[productType.selectedIndex].value;
+            if (productTypeSelected == 3) {
+                limitInput.hidden = false;
+                amountInput.hidden = true;
+            } else {
+                limitInput.hidden = true;
+                amountInput.hidden = false;
+                console.log(amountInput)
+            }
         }
     }
-}
+});
